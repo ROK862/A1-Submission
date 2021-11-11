142fc8b8e14d03971da4d3ae045d291f79e3edcd
ROK862 <88340948+ROK862@users.noreply.github.com>
2021-11-11 21:55:26 +0200

    Timer workflow state.

--- src/components/generic/TimerDisplay.js
+++ src/components/generic/TimerDisplay.js
@@ -4,13 +4,28 @@ import { AppContext } from "../../context/AppProvider";
 import { APP_RENDER_STATES } from "../../context/Consts"
 
 function TimerDisplay () {
-  const { currentTimer, rounds, formattedTime:displayTime, setTimerStatus:onPauseTimer, onSkipRound } = useContext(AppContext);
+  const { currentTimer, workflowState, rounds, formattedTime:displayTime, setTimerStatus:onPauseTimer, onSkipRound } = useContext(AppContext);
 
 
   const renderState = () => {
 
     switch (currentTimer) {
       case APP_RENDER_STATES.XY:
+        return (
+          <div className="Content-Wraper">
+            <div className='Timer-Tittle'>
+              {workflowState}
+            </div>
+            <div className='StopWatch'>
+              {displayTime}
+            </div>
+            <pre className='Rounds-Preview'>
+              Rounds: {rounds}
+            </pre>
+            <AnchorButton name='Pause' onClick={() => onPauseTimer("paused")} />
+            <AnchorButton name='Next Round' onClick={() => onSkipRound()} />
+          </div>
+        );
       case APP_RENDER_STATES.TABATA:
         return (
         <div className="Content-Wraper">
